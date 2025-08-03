de4ded44988eb65d1595c493ac9ea7922c354b28
parizval <anmolgoyal@gmail.com>
2025-08-03 13:24:33 +0530

    added config file

---
+++ config/utils.ts
@@ -0,0 +1,10 @@
+export const sepoliaLimitOrderContract =
+  "0xBb0903f2112d60B322117573848dc43edA5a5dE3";
+
+export const sepoliaEscrowFactoryContract =
+  "0x4c4FDAdd93748a776459cD800440519Fa198d147";
+
+export const sepoliaUSDCContract = "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238";
+
+export const etherlinkUSDCContract =
+  "0x4C2AA252BEe766D3399850569713b55178934849";
